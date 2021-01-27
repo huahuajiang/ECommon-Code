741de27a928edcbbcc55940bf7285c5f6c722de0
liujunhua <980929491@qq.com>
2021-01-27 17:59:30 +0800

    “2021/1/27；

--- ECommon/Storage/ChunkManager.cs
+++ ECommon/Storage/ChunkManager.cs
@@ -1,7 +1,9 @@
 ﻿using ECommon.Components;
 using ECommon.Logging;
+using ECommon.Scheduling;
 using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace ECommon.Storage
@@ -12,6 +14,28 @@ namespace ECommon.Storage
         private readonly object _lockObj = new object();
         private readonly ChunkManagerConfig _config;
         private readonly IDictionary<int, Chunk> _chunks;
+        private readonly string _chunkPath;
+        private readonly IScheduleService _scheduleService;
+        private readonly bool _isMemoryMode;
+        private int _nextChunkNumber;
+        private int _uncachingChunks;
+        private int _isCachingNextChunk;
+        private ConcurrentDictionary<int, BytesInfo> _byteWriteDict;
+        private ConcurrentDictionary<int, CountInfo> _fileReadDict;
+        private ConcurrentDictionary<int, CountInfo> _unmanagedReadDict;
+        private ConcurrentDictionary<int, CountInfo> _cachedReadDict;
 
+        class BytesInfo
+        {
+            public long PreviousBytes;
+            public long CurrentBytes;
+
+            public long UpgradeBytes()
+            {
+                var incrementBytes = CurrentBytes - PreviousBytes;
+                PreviousBytes = CurrentBytes;
+                return incrementBytes;
+            }
+        }
     }
 }
