8839bad3f698912d5980d579d1a725889a50785d
liujunhua <980929491@qq.com>
2021-02-05 17:55:51 +0800

    2021/2/5

--- EQueue/Broker/IQueueStore.cs
+++ EQueue/Broker/IQueueStore.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EQueue.Protocols.Brokers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,6 +11,18 @@ namespace EQueue.Broker
         void Start();
         void Shutdown();
         IEnumerable<string> GetAllTopics();
-        Queue
+        Queue GetQueue(string topic, int queueuId);
+        int GetAllQueueCount();
+        IEnumerable<Queue> GetAllQueues();
+        IList<TopicQueueInfo> GetTopicQueueInfoList(string topic = null);
+        long GetTotalUnConusmedMessageCount();
+        bool IsTopicExist(string topic);
+        bool IsQueueExist(QueueKey queueKey);
+        bool IsQueueExist(string topic, int queueId);
+        long GetQueueCurrentOffset(string topic, int queueId);
+        long GetQueueMinOffset(string topic, int queueId);
+        void AddQueue(string topic);
+        void DeleteQueue(string topic, int queueId);
+        void SetProducerVisible()
     }
 }
